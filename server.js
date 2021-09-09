cfafa0a9eb59c0cc208a2269b7d90d24c8fd240d
BABA <tanishkbohare.met18@itbhu.ac.in>
2021-09-09 20:14:03 +0530

    bigfix

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use('/api/v1/users', users)
 app.use('/api/v1/tasks', tasks)
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
+    // app.use(express.static('client/build'))
     
-    app.get('*', (req, res) => {
-        console.log('IT WORKS!!')
-        //res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+    // app.get('*', (req, res) => {
+    //     console.log('IT WORKS!!')
+    //     //res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    // })
 }
 
 const PORT= process.env.PORT || 5000
